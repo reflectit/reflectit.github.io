5c60837cfa9640169074e4fd6a9541846686a19b
reflectit <reflect0it@gmail.com>
2025-03-09 05:30:57 +0800

    Update verlog.js

--- src/js/verlog.js
+++ src/js/verlog.js
@@ -1,4 +1,4 @@
-import { app } from "../../src/js/firebase-config.js";
+import { app } from "./firebase-config.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const auth = getAuth(app);
@@ -7,7 +7,7 @@ onAuthStateChanged(auth, (user) => {
   if (!user) {
     alert("You must be logged in to access this page.");
     location.href = "auth/logins.html";
+  } else {
+    location.href = "secured/dashboards.html";
   }
-  else
-    location.href = "secured/dashboards.html"
 });
