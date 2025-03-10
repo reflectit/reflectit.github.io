dfe1633d82b9e56b16077bdc494770604788892d
reflectit <reflect0it@gmail.com>
2025-03-11 03:07:50 +0800

    Update firebase-config.js

--- src/js/firebase-config.js
+++ src/js/firebase-config.js
@@ -1,15 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAPcTIfGYQMjBa02BDbQ8fsOCZK5ASeCTA",
   authDomain: "reflect-it-57f45.firebaseapp.com",
   projectId: "reflect-it-57f45",
-  storageBucket: "reflect-it-57f45.firebasestorage.app",
+  storageBucket: "reflect-it-57f45.appspot.com", // Fixed the domain
   messagingSenderId: "26333738927",
   appId: "1:26333738927:web:0061534f4252a0b528a15a",
   measurementId: "G-QVWE3CJSV6"
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app); // Initialize Firestore
 
-export { app };
+export { app, db };
